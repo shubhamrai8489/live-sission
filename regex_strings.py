f3d7ac0fb87d9956b0e4b838dd312e2a0514b8d2
shubham Rai <shubhamrai8361@gmail.com>
2025-06-29 20:17:19 +0530

    session 14

---
+++ regex_strings.py
@@ -0,0 +1,19 @@
+message="Hello to Devops World!"
+
+#1. Remove leading and Trainiling Spaces
+print("1. strip():",message.strip())
+
+#2 convert to Uppercase
+print("2. upper():",message.upper())
+
+#3convert to lowercase
+print ("3. lower():",message.lower())
+
+#4 replace a world
+print("4. replace():",message.lower())
+
+#5 check if string starts with a word
+print("5. startswith('Hello'):",message.strip().startswith("Hello"))
+
+#6 check if string ends with a word
+print("6. endswith('Word'):",message.strip().endswith("World!"))
